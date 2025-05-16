491be545abdc46b01384dfff036eb743314a9f66
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-16 10:24:50 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  "fullName": "dasd ",
+  "email": "as@ss.com",
+  "giveawayName": "Burger Jabs Feast",
+  "submittedAt": "2025-05-13T18:42:46.192Z",
+  "deploymentInfo": {
+    "repoName": "openhouse",
+    "repoUrl": "https://github.com/arslvn93/openhouse",
+    "tag": "Open House 168 Abbey",
+    "netlifyUrl": "https://openhousetester.netlify.app/",
+    "netlifyId": "547dc1ef-2c83-4d08-815f-344d40fc3ca2"
+  }
+};
