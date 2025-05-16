a0a887344fb7318492b54a95fb7b2985a82f82d7
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-16 13:28:15 -0400

    Update Site

--- config.js
+++ config.js
@@ -12,16 +12,16 @@ const config = {
     "phone": "15874391618",
     "email": "amy@caughlinrealestate.ca",
     "bio": "A former risk manager turned Realtor, proptech co-founder, podcast host, and passionate community advocate. I help busy professionals and families find homes that truly fit their lives, while working behind the scenes to make real estate better for everyone involved. With a background in risk management, I bring a strategic lens to every transaction, ensuring my clients make informed, confident decisions. I’m also the co-founder of Jazzberry, a platform designed by realtors, for realtors. A proud Maritimer, I lead with authenticity, hospitality, and heart. Whether I’m running along the Bow River, adventuring in the mountains with my family, or hosting FutureRE — The Future of Real Estate, I’m always driven by curiosity, connection, and impact.",
-    "photoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/d43b40bf-8ed2-49c3-a704-34908837cf96/SueMoodiePhotography_CalgaryBrandPhotography_AmyCaughlin_StudioSession_60_Edit.jpg",
+    "photoUrl": "https://static.showit.co/800/TUEDlhwVYAO5v1iU87fE1w/264146/headshot.jpg",
     "social": {
       "facebook": "https://www.facebook.com/caughlinrealestate/",
-      "instagram": "https://www.instagram.com/alewi854",
-      "website": "https://www.welpneedthis.com"
+      "instagram": "https://www.instagram.com/caughlin.realestate/",
+      "website": "https://caughlinrealestate.ca/"
     }
   },
   "brokerage": {
     "name": "Greater Property Group",
-    "logoUrl": "",
+    "logoUrl": "https://static.showit.co/400/I76PcfBRdfuEObejOPwzPQ/264146/gpg-no-tagline-logo-white.png",
     "address": "5940 Macleod Trl SW #500, Calgary, AB T2H 2G4"
   },
   "openHouse": {
@@ -29,35 +29,35 @@ const config = {
     "bundleItems": [
       {
         "icon": "fas fa-ruler-combined",
-        "text": "Architectural Floor Plan: https://youriguide.com/142_valley_glen_heights_nw_calgary_ab/doc/floorplan_imperial_en_u.pdf"
+        "text": "Architectural Floor Plan"
       },
       {
         "icon": "fas fa-star",
-        "text": "Benefit: Amazing back yard, expansive 44x146-foot lot (7,588 sq ft) backing onto nature reserve & Bow River Pathway"
+        "text": "Neighbourhood Guide"
       },
       {
         "icon": "fas fa-check-circle",
-        "text": "Benefit: Golf course community"
+        "text": "Market Stats"
       },
       {
         "icon": "fas fa-car",
-        "text": "Oversized double garage"
+        "text": "School District Information"
       },
       {
         "icon": "fas fa-key",
-        "text": "Detached home, 3 bedrooms, 4 baths"
+        "text": "Financing Resources"
       },
       {
         "icon": "fas fa-home",
-        "text": "Listing price: $1,050,000"
+        "text": "Utility Cost Estimates"
       },
       {
         "icon": "fas fa-map-location-dot",
-        "text": "Neighbourhood: Valley Ridge - 50% green space, ravines, golf, Bow River, outdoor amenities"
+        "text": "Property Feature List"
       },
       {
         "icon": "fas fa-image",
-        "text": "Property Listing: https://www.realtor.ca/real-estate/28293039/142-valley-glen-heights-nw-calgary-valley-ridge"
+        "text": "Home Inspection Tips"
       }
     ]
   },
